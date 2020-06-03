e1a940ac99e2ba7a22e004474e4fdefe653be058
Sandra Alpizar <sandraalpizar@sandras-mbp.lan>
2020-06-03 12:34:24 -0700

    update

--- Exercise Files/Ch02/02_03/start/react-is-fun/src/index.js
+++ Exercise Files/Ch02/02_03/start/react-is-fun/src/index.js
@@ -1,12 +1,77 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+var style = {
+  backgroundColor: 'orange',
+  color: 'white',
+  fontFamily: 'Arial'
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+var liStyle = {
+  backgroundColor: 'blue',
+  color: 'white',
+  fontFamily: 'Arial'
+}
+
+//JSX Exampple
+ReactDOM.render(
+  <div style={style}>
+    <h1 id = 'heading-element'>Hello World!</h1>
+    <p>we are glad you are here</p>
+  </div>,
+  document.getElementById('root')
+)
+
+/*
+# Example 1
+const title = React.createElement(
+  'h1',
+  {id: 'title', className: 'header', style: style},
+  'Hello World'
+)
+*/
+
+/*
+# Example 2
+const title = React.createElement(
+  'ul',
+  {id: 'title', className: 'header', style: style},
+  React.createElement(
+    'li',
+    {id: 'li1'},
+    '1 item'
+  ),
+  React.createElement(
+    'li',
+    {id: 'li2'},
+    '2 item'
+  )
+)
+*/
+
+/*
+# Example 3
+var li1 = React.createElement('li', {id: 'li1', style: liStyle}, '1 item');
+var li2 = React.createElement('li', {id: 'li2'}, '2 item');
+var li3 = React.createElement('li', {id: 'li3', style: liStyle}, '3 item');
+var li4 = React.createElement('li', {id: 'li4'}, '4 item');
+var li5 = React.createElement('li', {id: 'li5', style: liStyle}, '5 item');
+
+const title = React.createElement(
+  'ul',
+  {id: 'title', className: 'header', style: style},
+  li1,
+  li2,
+  li3,
+  li4,
+  li5
+)
+*/
+
+/*
+# Example 1 Render
+ReactDOM.render(
+  title,
+  document.getElementById('root')
+)
+*/
